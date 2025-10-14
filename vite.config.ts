082f894fc8a542dd19bbf865b65cadf5a851e6e9
me-RK <rithikkrisna0@gmail.com>
2025-10-14 17:47:15 +0530

    fix: GitHub Pages base path and meta tags

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(() => {
 
   return {
     plugins: [react()],
-    base,
+    base: '/Smart-Water-Tank-PWA/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
