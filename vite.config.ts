2bd378047e52a55ee03a9012daa0a59d0eb5ddd9
me-RK <rithikkrisna0@gmail.com>
2025-10-14 17:50:48 +0530

    refactor: Simplify base path configuration in Vite setup.

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig(() => {
 
   return {
     plugins: [react()],
-    base: '/Smart-Water-Tank-PWA/',
+    base,
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
